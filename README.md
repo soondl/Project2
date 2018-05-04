38f0a14914a967d6fa268da25d358546853112a0
cocoapi <eksrns22tp@gmail.com>
2018-05-04 20:39:26 +0900

    Update README and source code

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # SE_project2
+## 1.시작하기
+```
+git clone https://github.com/soondl/SE_project2.git
+cd SE_project2
+git checkout -b [branch_name]
+```
+으로 개별 브랜치 생성후 작업해주세요
+
+## 2.출력예시
+```
+size compare
+text2 is bigger
+
+block compare
+text2 is bigger
+
+date compare
+same date
+
+time compare
+same time
+```
+
+## 3.참고자료
+[stat(2) manpage](http://man7.org/linux/man-pages/man2/stat.2.html)  
+[struct tm](http://www.cplusplus.com/reference/ctime/tm/)  
+
---
+++ src/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+
+struct stat stat1, stat2;
+struct tm *time1, *time2;
+
+void filestat1(void);
+void filestat2(void);
+void filetime1(void);
+void filetime2(void);
+void sizecmp(void);
+void blockcmp(void);
+void datecmp(void);
+void timecmp(void);
+
+int main(void)
+{
+	filestat1();
+	filestat2();
+	filetime1();
+	filetime2();
+	sizecmp();
+	blockcmp();
+	datecmp();
+	timecmp();
+}
+
+//파일 1의 정보를 가져오는 함수 작성
+void filestat1(void)
+{
+  const char* filename = "./text1";
+
+  if (stat(filename, &stat1)) {
+    puts("invalid filename");
+  }
+}
+
+//파일 2의 정보를 가져오는 함수 작성
+void filestat2(void)
+{
+  const char* filename = "./text2";
+
+  if (stat(filename, &stat2)) {
+    puts("invalid filename");
+  }
+}
+
+//파일 1의 시간 정보를 가져오는 함수 작성
+void filetime1(void)
+{
+	static struct tm t1;
+	t1 = *(localtime(&stat1.st_mtime));
+	time1 = &t1;
+}
+
+//파일 2의 시간 정보를 가져오는 함수 작성
+void filetime2(void)
+{
+	static struct tm t2;
+	t2 = *(localtime(&stat2.st_mtime));
+	time2 = &t2;
+}
+
+//두 개의 파일 크기를 비교하는 함수 작성
+void sizecmp(void)
+{
+}
+
+//두 개의 파일 블락 수를 비교하는 함수 작성
+void blockcmp(void)
+{
+}
+
+//두 개의 파일 수정 날짜를 비교하는 함수 작성
+void datecmp(void)
+{
+}
+
+//두 개의 파일 수정 시간을 비교하는 함수 작성
+void timecmp(void)
+{
+}
